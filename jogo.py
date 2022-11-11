dccaca1e28015f24107a69b1eb9146e339aac864
brunobraghette <brunobraghette@gmail.com>
2022-11-11 08:40:27 -0300

    Tratamento da base de dados

--- jogo.py
+++ jogo.py
@@ -159,4 +159,18 @@ quest = [{'titulo': 'Qual o resultado da operação 57 + 32?',
           'correta': 'D'}
         ]
 
+def transforma_base(lista):
+    dicio_final = {}
+    for i in range(len(lista)):
+        dificuldade = lista[i]['nivel']
+        print(dificuldade)
+        if dificuldade in dicio_final.keys():
+            if lista[i] not in dicio_final.values():
+                dicio_final[dificuldade] += [lista[i]]
+        if dificuldade not in dicio_final.keys():
+            if lista[i] not in dicio_final.values():
+                dicio_final[dificuldade] = [lista[i]]
+    return dicio_final
+
+
 base = transforma_base(quest)
